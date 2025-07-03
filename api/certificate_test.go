80814362cb8752d7c66c31a1b3afd810c27dd50e
François Gouteroux <francois.gouteroux@gmail.com>
2025-07-03 09:06:35 +0200

    fix: test

--- api/certificate_test.go
+++ api/certificate_test.go
@@ -203,7 +203,7 @@ func TestAPICertificateMetadataHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	expected := `[{"domain":"testfgx.example.com","issuer":"pebble","bundle":false,"expires":"","fingerprint":"","owner":"testuser","csr":"","labels":"","encryption":"","serial":""}]`
+	expected := `[{"domain":"testfgx.example.com","issuer":"pebble","bundle":false,"expires":"","fingerprint":"","owner":"testuser","csr":"","labels":"","encryption":"","serial":"","key_type":""}]`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
