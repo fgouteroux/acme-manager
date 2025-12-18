0de329b1be7f637862a6c6fd792c7b4532e36f48
François Gouteroux <francois.gouteroux@gmail.com>
2025-12-18 11:17:58 +0100

    fix: client - Add debounce mechanism to fsnotify watcher to coalesce rapid events

--- client/watcher.go
+++ client/watcher.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -13,6 +14,8 @@ import (
 	"github.com/fgouteroux/acme-manager/metrics"
 	"github.com/fgouteroux/acme-manager/restclient"
 )
+
+const debounceDelay = 500 * time.Millisecond
 
 func WatchCertificateChange(logger log.Logger, interval time.Duration, configPath string, acmeClient *restclient.Client) {
 	// create a new Ticker
@@ -57,17 +60,29 @@ func WatchCertificateEventChange(logger log.Logger, configPath string, acmeClien
 		os.Exit(1)
 	}
 
+	// Debounce mechanism to coalesce rapid file events into a single check
+	var debounceTimer *time.Timer
+	var debounceMu sync.Mutex
+
 	for {
 		select {
 		case event := <-watcher.Events:
 			// listen for CREATE/RENAME/WRITE events from original filename
 			if (event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create || event.Op&fsnotify.Rename == fsnotify.Rename) && event.Name == fileName {
-				_ = level.Info(logger).Log("msg", fmt.Sprintf("modified file: %s", configPath))
+				_ = level.Debug(logger).Log("msg", fmt.Sprintf("file event detected: %s (op: %s)", configPath, event.Op))
 
-				metrics.IncCertificateConfigReload()
-
-				// Compare and create/update certificate from config file to remote server
-				go CheckCertificate(logger, configPath, acmeClient)
+				debounceMu.Lock()
+				// Reset the timer on each event to wait for events to settle
+				if debounceTimer != nil {
+					debounceTimer.Stop()
+				}
+				debounceTimer = time.AfterFunc(debounceDelay, func() {
+					_ = level.Info(logger).Log("msg", fmt.Sprintf("modified file: %s", configPath))
+					metrics.IncCertificateConfigReload()
+					// Compare and create/update certificate from config file to remote server
+					CheckCertificate(logger, configPath, acmeClient)
+				})
+				debounceMu.Unlock()
 			}
 		case err := <-watcher.Errors:
 			_ = level.Error(logger).Log("msg", "file watcher error", "err", err)
